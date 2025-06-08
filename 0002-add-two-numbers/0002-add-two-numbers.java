1bb68ca5afd3e64e8a3d3da470ee3191b4897f73
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-06-08 22:15:19 +0530

    Time: 1 ms (100%), Space: 44.5 MB (54.01%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -35,18 +35,4 @@ class Solution {
         }
         return ans;
     }
-
-    static ListNode reverseNode(ListNode head){
-        if(head==null || head.next==null) return head;
-        ListNode prev=null;
-        ListNode cur=head;
-        while(cur!=null){
-            ListNode next=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=next;
-        }
-        return prev;
-    }
-
 }
