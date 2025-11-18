5c1660b103c86d4b902a9f1a0f3dceedd153dc32
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-11-18 23:22:36 +0530

    Time: 2 ms (26.81%), Space: 61.2 MB (15.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,45 +1,31 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        if(n==0) return;
 
-            // solution-1 using reversal array algorithm
-         int n = nums.length;
-
-        // handle the case where k> array length n
         k = k%n;
+        int i,j;
 
-        // reverse the entire element
-        reverse(nums,0,n-1);
+        // reverse last k element
+        for(i=n-k,j=n-1;i<j;i++,j--){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-        // reverse the k element
-        reverse(nums,0,k-1);
+        //reverse first n-k element
+        for(i=0,j=n-k-1;i<j;i++,j--){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-        // reverse the n-k array
-        reverse(nums,k,n-1);
-
-    }
-
-    static void reverse(int[] nums,int left,int right){
-        while(left<right){
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+        // reverse the whole array
+        for(i=0,j=n-1;i<j;i++,j--){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
 
     }
-
-            // solution-2 simple rotate one by one 
-
-    //    int n = nums.length;
-
-    //    for(int i=0;i<k;i++){
-    //      int first = nums[0];
-
-    //     for(int j=0;j<nums.length-1;j++){
-    //         nums[j] = nums[j+1];
-    //     }
-    //     nums[n-1] = first;
-
-    //    }
-    }
+}
