e3018a843ac82d3e5531874145fb464a14e76cb7
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-12-03 00:35:28 +0530

    Time: 1 ms (99.27%), Space: 47.5 MB (6.62%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0;
+        int maxReachable=0;
+        int currentJumpsEnd=0;
+
+        for(int i=0;i<nums.length-1;i++){
+            maxReachable = Math.max(maxReachable,i+nums[i]);
+
+            if(i == currentJumpsEnd){
+                jumps++;
+
+                currentJumpsEnd=maxReachable;
+
+            }
+
+        }
+
+        return jumps;
+
+    }
+}
